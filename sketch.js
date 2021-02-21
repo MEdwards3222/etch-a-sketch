e330dfd4a4812ddf579a4a024aba178da899864c
MEdwards3222 <medwards3222@gmail.com>
2021-02-20 23:02:25 -0500

    js changes

--- sketch.js
+++ sketch.js
@@ -1,9 +1,27 @@
-const gridContainer = document.getElementById("container");
-gridContainer.style.display("grid");
+const gridContainer = document.querySelector("#gridContainer");
 
-let gridDiv = document.createElement("div");
-gridDiv.setAttribute("id='grid'");
 
-function createGrid() {
+window.addEventListener("load", setDefaultGrid);
+
+
+function setDefaultGrid() {
+  setGridSize(16);
+  fillGrid(16);
+}
+
+function setGridSize(size) {
+  gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+}
+
+function fillGrid(size) {
+  for (let i = 0; i < size * size; i++) {
+    const gridElement = document.createElement("div");
+    gridElement.classList = "grid-div";
+    gridElement.addEventListener("mouseover", changeColor);
+    gridContainer.appendChild(gridElement);
+  }
+}
+
+function changeColor() {
 
 };
