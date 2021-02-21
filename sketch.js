d848661ee0bbd2fba2e88f00320bea94f78b71fe
MEdwards3222 <medwards3222@gmail.com>
2021-02-20 23:00:59 -0500

    Reset Progress - starting from scrach on js file

---
+++ sketch.js
@@ -0,0 +1,9 @@
+const gridContainer = document.getElementById("container");
+gridContainer.style.display("grid");
+
+let gridDiv = document.createElement("div");
+gridDiv.setAttribute("id='grid'");
+
+function createGrid() {
+
+};
